0e95b02e7b1bccce5f725911e987447539780535
angelou <anjacelis21@gmail.com>
2017-08-10 11:45:35 +0800

    sample

--- backend/views/customer/index.php
+++ backend/views/customer/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     
     <div class="box box-warning">
         <div class="box-header with-border">
-            <h3 class="box-title">Search</h3>
+            <h3 class="box-title">Searchss</h3>
         </div>
         <div class="box-body">
             <div class="col-md-12">
